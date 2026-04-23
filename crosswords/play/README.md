1fff672432878805a6673307b0cd3583d71af2d5
Brian Shaginaw <bshaginaw120@gmail.com>
2026-04-23 15:10:38 -0400

    Add a build script for /crosswords/play

---
+++ crosswords/play/README.md
@@ -0,0 +1,6 @@
+# Build Script for [/crosswords/play](https://orez-share.github.io/crosswords/play)
+
+Assumes you've got the [variant crossword player](https://github.com/orez-share/variant-crossword-player/) project code checked out sibling to the orez-share.github.io project.
+
+The `./build.sh` script will build the variant crossword player code and replace the existing version of the app with the new build.
+It will also commit the changes, but will not push them.
---
+++ crosswords/play/build.sh
@@ -0,0 +1,33 @@
+set -euxo pipefail
+shopt -s dotglob
+PROJECT_DIR=$( git rev-parse --show-toplevel )
+cd "${PROJECT_DIR}"
+
+# Build the app
+APP_DIR="../variant-crossword-player"
+pushd "${APP_DIR}"
+XWORD_BASE_PATH="/crosswords/play" npm run build
+popd
+
+# If the worktree doesn't exist for some reason, make it
+if [ ! -d "gh-pages" ]; then
+  git worktree add gh-pages gh-pages
+fi
+
+# Remove the old build from our site, copy over the new build
+PAGE_PATH="${PROJECT_DIR}/gh-pages/crosswords/play/"
+rm -rf "$PAGE_PATH"
+mkdir -p "$PAGE_PATH"
+cp -r "${APP_DIR}/build"/* "$PAGE_PATH"
+
+# Also remove the test xwords
+pushd "${PAGE_PATH}"
+rm -rf "/puz"
+
+# Commit, but don't push
+git add .
+date=`date '+%F %H:%M:%S'`
+git commit -m "Publish /crosswords/play $date" || $(exit 0)
+
+set +x
+echo -e "\n\x1B[32m/crosswords/play built! Don't forget to \`git push\` the /gh-pages directory once you're satisfied with your changes\x1B[0m"
